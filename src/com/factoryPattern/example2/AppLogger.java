5c2690308b26ae9162fab64e2b12a743e28242b4
sai <kiranthewarrior@gmail.com>
2015-12-07 01:13:22 +0530

    Factory Pattern Method example2

---
+++ src/com/factoryPattern/example2/AppLogger.java
@@ -0,0 +1,6 @@
+package com.factoryPattern.example2;
+
+public interface AppLogger {
+
+	public void log(String logMsg);
+}
---
+++ src/com/factoryPattern/example2/FileLogger.java
@@ -0,0 +1,10 @@
+package com.factoryPattern.example2;
+
+public class FileLogger implements AppLogger{
+
+	public void log(String logMsg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
